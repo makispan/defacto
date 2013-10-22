bafbef681e92660c5d3196f6d012afba690c5b12
Makis Panagopoulos <makis@webmediastudio.gr>
2013-10-22 15:43:02 +0300

    defacto

---
+++ resources/themes/uber-naked/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <title>UberGallery</title>
+    <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/images/favicon.png" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/rebase-min.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
+    <?php echo $gallery->getColorboxStyles(1); ?>
+
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <?php echo $gallery->getColorboxScripts(); ?>
+
+    <?php file_exists('googleAnalytics.inc') ? include('googleAnalytics.inc') : false; ?>
+    <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:200' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+    <div class="intro"><img src="defacto-contest.jpg"></div>
+
+    <h1>Recent Entries</h1>
+
+    <?php
+        $galleryArray['relText'] = 'colorbox';
+        echo $gallery->readTemplate('templates/defaultGallery.php', $galleryArray);
+    ?>
+
+</body>
+
+</html>
